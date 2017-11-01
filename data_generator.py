88b5cbf54cee528f44983ca09bad8ebea5708125
eeddaann <edan100@gmail.com>
2017-11-01 16:28:10 +0200

    generates random data

--- data_generator.py
+++ data_generator.py
@@ -1,23 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from constants import groups
 
-vac_factor = 0.2
-time = np.linspace(0,100,100)
+def generate(vac_factors):
+    eqs = []
+    for group in groups:
+        if group.state is 'i':
+            eqs.append(np.random.uniform(0.1,0.9,100)*vac_factors[str(group.group)])
+        else:
+            np.random.uniform(0.1,0.9,100)
 
-s = np.random.poisson(5, 100)*time
-i = np.random.poisson(5*vac_factor, 100)*time
-r = np.random.poisson(5, 100)*time
+    total = sum(eqs)
+    eqs = [eq/total for eq in eqs]
+    # print(sum(eqs))
+    # for eq in eqs:
+    #     plt.plot(eq)
+    #
+    # plt.show()
+    return eqs
 
-total = s+i+r
-s = s/total
-i = i/total
-r = r/total
+print(generate({'g1':0.01,'g2':0.05}))
 
-print(s)
-print(i)
-print(r)
-print(s+i+r)
-plt.plot(s)
-plt.plot(i)
-plt.plot(r)
-plt.show()
+no_vac = generate({'g1':1,'g2':1})
+vac = generate({'g1':0.0001,'g2':0.0001})
+print(sum(sum(np.array(no_vac)-np.array(vac))))
