cd638b2b057c62096a8050d69365c940629884d4
eeddaann <edan100@gmail.com>
2017-11-28 00:32:46 +0200

    Update simulation

--- traffic-simulation.py
+++ traffic-simulation.py
@@ -1,11 +1,24 @@
-import numpy as np
-population_distribution = [0.11,0.33,0.41,0.15]
 
-roads = []
+from numpy.random import choice
+from numpy import array
+
+population_distribution = [0.11,0.33,0.41,0.15]
+driver_distribution = [0,0.0196,0.6979,0.2825]
+roads = array([14.9,9.8,3.4,12.9,26.2,6.5,17,25.6,30.7,1.9])*500 # direction
+elements = ['0-4', '5-19', '20-49', '50+']
+d = dict(zip(elements, [0]*4))
 def sample_car():
-    # sample driver
-    # sample other
-    pass
+    d[choice(elements, p=driver_distribution)] += 1
+    d[choice(elements, p=population_distribution)] += 1
+    d[choice(elements, p=population_distribution)] += 1
 
 for road in roads:
-    sample_car()
+    for i in range(int(road)):
+        sample_car()
+
+print(d)
+print("total: "+str(sum(d.values())))
+
+# output:
+# {'0-4': 16157, '20-49': 113401, '5-19': 50604, '50+': 43188}
+# total: 223350
